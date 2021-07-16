9d7ddd929cb1a66c48892fe89df86f89b5b00938
Rafael Henrique Lisot <rafael.lisot@unoesc.edu.br>
2021-07-16 18:28:47 -0300

    sistema de registro e login completos

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -3,8 +3,118 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['create', 'login', 'unauthorized']]);
+    }
+
+    public function unauthorized() {
+        return rsponse()->json([
+            'error' => 'Não autorizado'
+        ], 401);
+    }
+
+    public function create(Request $request) {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!$validator->fails()) {
+            $name = $request->input('name');
+            $email = $request->input('email');
+            $password = $request->input('password');
+
+            $emailExists = User::where('email', $email)->count();
+
+            if ($emailExists === 0) {
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+
+                $newUser = new User();
+                $newUser->name = $name;
+                $newUser->email = $email;
+                $newUser->password = $hash;
+                $newUser->save();
+
+                $token = auth()->attempt([
+                    'email' => $email,
+                    'password' => $password
+                ]);
+
+                if (!$token) {
+                    $array['error'] = 'Ocorreu um erro interno';
+
+                    return $array;
+                }
+
+                $info = auth()->user();
+                $info['avatar'] = url('media/avatars/'.$info['avatar']);
+                $array['data'] = $info;
+                $array['token'] = $token;
+            } else {
+                $array['error'] = 'E-Mail já cadastrado';
+
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Dados Incorretos!!!';
+            
+            return $array;
+        }
+        
+        return $array;
+    }
+
+    public function login(Request $request) {
+        $array = ['error' => ''];
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $token = auth()->attempt([
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        if (!$token) {
+            $array['error'] = 'Usuário e/ou senha incorretos';
+
+            return $array;
+        }
+
+        $info = auth()->user();
+        $info['avatar'] = url('media/avatars/'.$info['avatar']);
+        $array['data'] = $info;
+        $array['token'] = $token;
+
+        return $array;
+    }
+
+    public function logout() {
+        auth()->logout();
+
+        return ['error' => ''];
+    }
+
+    public function refresh() {
+        $array = ['error' => ''];
+
+        $token = auth()->refresh();
+
+        $info = auth()->user();
+        $info['avatar'] = url('media/avatars/'.$info['avatar']);
+        $array['data'] = $info;
+        $array['token'] = $token;
+
+        return $array;
+    }
 }
